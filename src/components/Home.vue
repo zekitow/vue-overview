4701af96e1e6b69c2f143bd131482b0f96864af3
José Ribeiro <zeca@digup.io>
2017-06-21 21:06:51 -0300

    Adding new posts.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,11 +4,23 @@
     <p>{{ msg }}</p>
 
     <div class="posts">
+      <div>
+        <form v-on:submit="addPost">
+          <input type="text" placeholder="title" v-model="newPost.title" /> <br>
+          <textarea rows="5" placeholder="body" v-model="newPost.body"></textarea>
+          <button>Save</button>
+        </form>
+      </div>
+      <hr>
       <div v-for="post in posts">
-        <a v-on:click="remove(post)">Remove</a>
-        <h2>{{ post.title }}</h2>
-        <p>{{ post.body }}</p>
-        
+        <div class="post">
+          <a v-on:click="removePost(post)">Remove</a>
+          <h2>{{ post.title }}</h2>
+          <p>{{ post.body }}</p>
+        </div>
+      </div>
+      <div class="message" v-if="posts.length == 0">
+        <p>Sorry, no posts yet.</p>
       </div>
     </div>
 
@@ -21,11 +33,19 @@ export default {
   data () {
     return {
       msg: 'This is the home message',
-      posts: []
+      posts: [],
+      newPost: {}
     }
   },
   methods: {
-    remove: function(post) {
+    addPost: function(e) {
+      e.preventDefault();
+      this.posts.push({
+        title: this.newPost.title,
+        body: this.newPost.body
+      })
+    },
+    removePost: function(post) {
       this.posts.splice(this.posts.indexOf(post), 1);
     }
   },
@@ -47,7 +67,21 @@ a {
   font-weight: bold;
   color: blue;
 }
+.message {
+  color: #DDD;
+  text-align: center;
+}
 .posts {
   text-align: left;
+}
+
+.posts .post {
+  border: 1px solid #CCC;
+  margin: 2em 0;
+  padding: 1em;
+}
+
+input, textarea {
+  width: 100%;
 }
 </style>
